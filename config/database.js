fda294ffb7475021139009f23d246b0382f4dba3
HoangPhuong <hoangphuong0813@gmail.com>
2023-05-13 10:21:39 +0700

    14052023

--- config/database.js
+++ config/database.js
@@ -2,5 +2,11 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const url = process.env.MONGOURL;
 
-const connect = mongoose.connect(url);
-module.exports = {connect};
+const connect = () => {
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+            console.log("Mongoose Connected");
+        });
+}
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const {connect} = require("./config/database");
 const cors = require("cors");
 const cloudinary = require('cloudinary');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const connect = require("./config/database");
 const PORT = process.env.PORT || 4000;
+
+const ProductRouter = require("./router/product");
+const AdminRouter = require("./router/admin");
+const CartRouter = require("./router/cart");
+const wishlistRouter = require("./router/wishlist");
+const OrderListRouter = require("./router/orderlist");
 
 app.use(cors());
 app.use(express.json());
@@ -21,24 +28,35 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const ProductRouter = require("./router/product");
-const AdminRouter = require("./router/admin");
-const CartRouter = require("./router/cart");
-const wishlistRouter = require("./router/wishlist");
-const OrderListRouter = require("./router/orderlist");
-
 app.use("/api/products", ProductRouter);
 app.use("/api/admin", AdminRouter);
 app.use("/api/cart", CartRouter);
 app.use("/api/wishlist", wishlistRouter);
 app.use("/api/orderlist", OrderListRouter);
 
-app.listen(PORT, async () => {
-    try {
-        await connect;
-        console.log(`http://localhost:${PORT}`);
-    } catch (error) {
-        console.log("connection failed");
-    }
-    console.log(`The Port is Running on ${PORT}`);
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Server is Running! 🚀');
+    });
+}
+
+connect();
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
 });
